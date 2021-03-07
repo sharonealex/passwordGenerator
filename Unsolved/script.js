948c3762044bb0b4a87bdc9c99071d29932791af
Sharon <sharonelizalex@gmail.com>
2021-03-08 10:55:43 +1100

    added function to prompt user input

--- Unsolved/script.js
+++ Unsolved/script.js
@@ -1,6 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//to check for user values
+function isTrue(str){
+  return (str === "Y" || str === "y");
+}
+
+function getPasswordCriteria(){
+//get user input criteria
+var  length = prompt("Enter length of password");
+var specialChar = prompt("To include special chars; Enter Y");
+var upperCase =  prompt("To include uppercase chars; Enter Y");
+var number = prompt("To include number; Enter Y");
+
+//save criteria object
+ passwordCriteria = {
+  length: length,
+  hasSpecialChar: isTrue(specialChar),
+  hasUpperCaseChar: isTrue(upperCase),
+  hasNumberChar: isTrue(number)
+}
+return passwordCriteria;
+}
+
+function generatePassword(){
+ var passwordCriteria =  getPasswordCriteria();
+
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
