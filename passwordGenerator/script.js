2a4915e5e8774ede18ca1c72cb7646605aea261a
Sharon <sharonelizalex@gmail.com>
2021-10-15 09:01:30 +1100

    added funtion to generate password

--- passwordGenerator/script.js
+++ passwordGenerator/script.js
@@ -1,19 +1,139 @@
-var generateButton = document.querySelector("button");
+var generateButtonEl = document.querySelector("#button");
+var passwordTextArealEl = document.querySelector("#password-text")
+var specialCharacters = [
+    '@',
+    '%',
+    '+',
+    '\\',
+    '/',
+    "'",
+    '!',
+    '#',
+    '$',
+    '^',
+    '?',
+    ':',
+    ',',
+    ')',
+    '(',
+    '}',
+    '{',
+    ']',
+    '[',
+    '~',
+    '-',
+    '_',
+    '.',
+  ];
+  
+  // Array of numeric characters to be included in password
+  var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  
+  // Array of lowercase characters to be included in password
+  var lowerCasedCharacters = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+  
+  // Array of uppercase characters to be included in password
+  var upperCasedCharacters = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
 
-console.log(generateButton)
 
-
-
-
-
-
-var writePassword = function(){
-    console.log("line 11")
-var passwordOutput = document.querySelector("#password-text");
-console.log(passwordOutput)
-//var password = generatePassword();
-passwordOutput.value = "test"
+var getUserInputs = function(){
+    var length = parseInt(prompt("please enter the length of the password", "8"));
+    if(length < 8 || Number.isNaN(length)){
+        alert ("entered length is either invalid number or less that 8")
+        return;    // break out of the program flow. And user has to start the process again.
+    }
+    var hasSpecialChars = prompt("should there be special chars?", "yes");
+    var hasLowerChars = prompt("should there be lower chars?", "yes");
+    var hasUpperChars = prompt("should there be upper chars?", "yes");
+    var hasNumbers = prompt("should there be numbers?", "yes");
+    return {
+        length: length,
+        hasLowerChars: hasLowerChars,
+        hasSpecialChars: hasSpecialChars,
+        hasUpperChars: hasUpperChars,
+        hasNumbers: hasNumbers
+    }
 }
 
 
-generateButton.addEventListener("click", writePassword);
+var generatePassword = function(){
+var passwordText = '';
+var userInputs = getUserInputs(); 
+var selectedTypesArray = [];
+if (userInputs.hasLowerChars) selectedTypesArray.push(lowerCasedCharacters);
+if (userInputs.hasNumbers) selectedTypesArray.push(numericCharacters);
+if (userInputs.hasUpperChars) selectedTypesArray.push(upperCasedCharacters);
+if (userInputs.hasSpecialChars) selectedTypesArray.push(specialCharacters)
+for(var i = 0; i < userInputs.length; i ++){
+var randomArrayIndex = Math.floor(Math.random() * selectedTypesArray.length);
+var randomElementIndex = Math.floor(Math.random() * selectedTypesArray[randomArrayIndex].length)
+passwordText += selectedTypesArray[randomArrayIndex][randomElementIndex];
+}
+
+return passwordText;
+}
+
+var writePassword = function(){
+   var password =  generatePassword();
+   passwordTextArealEl.value = password;
+}
+
+
+
+
+
+generateButtonEl.addEventListener("click", writePassword);
